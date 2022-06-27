913bdc3397ad68514f64b52adf469d5419b2baeb
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-06-27 13:36:02 +0300

    closures

--- js/script.js
+++ js/script.js
@@ -1,51 +1,19 @@
-// Arrays
-var array = new Array();
-array[0] = "Yaakov";
-array[1] = 2;
-array[2] = function (name) {
-  console.log("Hello " + name);
-};
-array[3] = {course: " HTML, CSS & JS"};
-
-console.log(array);
-array[2](array[0]);
-console.log(array[3].course);
+// Closures
+function makeMultiplier (multiplier) {
+  // var multiplier = 2;
+  function b() {
+    console.log("Multiplier is: " + multiplier);
+  }
+  b();
 
 
-// Short hand array creation
-var names = ["Yaakov", "John", "Joe"];
-console.log(names);
+  return (
+      function (x) {
+        return multiplier * x;
+      }
 
-for (var i = 0; i < names.length; i++) {
-  console.log("Hello " + names[i]);
+    );
 }
 
-names[100] = "Jim";
-for (var i = 0; i < names.length; i++) {
-  console.log("Hello " + names[i]);
-}
-
-var names2 = ["Yaakov", "John", "Joe"];
-
-var myObj = {
-  name: "Yaakov",
-  course: "HTML/CSS/JS",
-  platform: "Courera"
-};
-for (var prop in myObj) {
-  console.log(prop + ": " + myObj[prop]);
-}
-
-for (var name in names2) {
-  console.log("Hello " + names2[name]);
-}
-
-names2.greeting = "Hi!";
-
-for (var name in names2) {
-  console.log("Hello " + names2[name]);
-}
-
-
-
-
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(10)); // its own exec env
