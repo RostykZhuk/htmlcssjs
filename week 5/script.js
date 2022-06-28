0c762cedd4fea342dc4fb84bf35f3948a7c61326
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-06-28 21:11:54 +0300

    Handling events

--- week 5/script.js
+++ week 5/script.js
@@ -1,28 +1,40 @@
-// DOM manipulation
-// console.log(document.getElementById("title"));
-// console.log(document instanceof HTMLDocument);
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    function sayHello (event) {
+      this.textContent = "Said it!";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
 
-function sayHello () {
-  var name =
-   document.getElementById("name").value;
-   var message = "<h2>Hello " + name + "!</h2>";
-
-  // document
-  //   .getElementById("content")
-  //   .textContent = message;
-
-  document
-    .getElementById("content")
-    .innerHTML = message;
-
-  if (name === "student") {
-    var title = 
       document
-        .querySelector("#title")
-        .textContent;
-    title += " & Lovin' it!";
-    document
-        .querySelector("h1")
-        .textContent = title;
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", sayHello);
+
   }
-}
+);
+
+
+
+// document.querySelector("button")
+//   .onclick = sayHello;
+
+
+
