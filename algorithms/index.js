7517def283d27df0bdb309bbea88cf9b8da10700
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-08-28 17:35:40 +0300

    sorting algorithms

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,20 +1,43 @@
-const binary_search = (list, item) => {
-  let low = 0
-  let high = list.length - 1
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2)
-    let guess = list[mid]
-    if (guess == item) {
-      return mid
-    }
-    if (guess > item) {
-      high = mid - 1
-    } else {
-      low = mid + 1
+// const find_smallest = (arr) => {
+//   let smallest = arr[0]
+//   let smallest_index = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < smallest) {
+//       smallest = arr[i]
+//       smallest_index = i
+//     }
+//   }
+//   return smallest_index
+// }
+// const selection_sort = (arr) => {
+//   let newArr = []
+//   let length = arr.length
+//   for (let i = 0; i < length; i++) {
+//     let smallest = find_smallest(arr)
+//     newArr.push(arr.splice(smallest, 1)[0])
+//   }
+//   return newArr
+// }
+// console.log(selection_sort([5, 3, 6, 122121, 10])) // [2, 3, 5, 6, 10]
+
+const find_greatest = (arr) => {
+  let greatest = arr[0]
+  let greatest_index = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > greatest) {
+      greatest = arr[i]
+      greatest_index = i
     }
   }
-  return null
+  return greatest_index
 }
-const my_list = [1, 3, 5, 7, 9]
-console.log(binary_search(my_list, 9))
-console.log(binary_search(my_list, -1))
+const selection_sort = (arr) => {
+  let newArr = []
+  let length = arr.length
+  for (let i = 0; i < length; i++) {
+    let greatest = find_greatest(arr)
+    newArr.push(arr.splice(greatest, 1)[0])
+  }
+  return newArr
+}
+console.log(selection_sort([5, 3, 6, 122121, 10])) // [122121, 10, 6, 5, 3]
