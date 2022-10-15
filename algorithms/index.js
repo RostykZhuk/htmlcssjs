754a7aa2f41e123051fbe3ef91cc273897781bf0
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-10-15 21:44:23 +0300

    recurison training

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,23 +1,9 @@
-// recursion code
-let sum = addTo(10);
-console.log(sum);
+// recursive function to count elements in list
+function countElements(list) {
+    if (list.length === 0) {
+        return 0;
+    }
+    return 1 + countElements(list.slice(1));
+    }
 
-function addTo(n) {
-  if (n == 0) return 0;
 
-  return n + addTo(n - 1);
-}
-
-// default code
-var sum1 = addTo(10);
-console.log(sum1);
-
-function addTo(n) {
-  var sum1 = 0;
-
-  for (var i = 1; i <= n; i++) {
-    sum1 += i;
-  }
-
-  return sum1;
-}
