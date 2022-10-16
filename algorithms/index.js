f225bdec3784b564f27257fd841bc7d1e50b6299
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-10-16 23:22:11 +0300

    recursion Find maximum

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,9 +1,39 @@
-// recursive function to count elements in list
-function countElements(list) {
-    if (list.length === 0) {
-        return 0;
-    }
-    return 1 + countElements(list.slice(1));
+// Use a stack to find the maximum numeric value in an array of arrays
+function findMax2(arElements)
+{
+    var max = -Infinity;
+
+    // This is the stack on which will put the first array and then 
+    // all the other sub-arrays that we find as we traverse an array     
+    var arrays = [];
+
+    arrays.push(arElements);
+
+    // Loop as long as are arrays added to the stack for processing
+    while(arrays.length > 0)
+    {
+        // Extract an array from the stack
+        ar = arrays.pop();
+
+        // ... and loop through its elements
+        for(var i = 0; i < ar.length; i++)
+        {
+            var el = ar[i];
+
+            // If an element is of type array, we'll add it to stack
+            // to be processed later
+            if ( Array.isArray(el) )
+            {
+                arrays.push(el);
+                continue;
+            }
+
+            if ( el > max )
+            {
+                max = el;
+            }
+        }
     }
 
-
+    return max;
+}
