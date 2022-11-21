9c97b2d66880cdae038e34927b802e38921ba26f
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-21 23:36:30 +0200

    set the prototype of subtype

--- js_practice/index.js
+++ js_practice/index.js
@@ -1,54 +1,68 @@
-//  function constructor
-function Bird(name) {
-  this.name = name; //own property
-}
+// //  function constructor
+// function Bird(name) {
+//   this.name = name; //own property
+// }
 
-Bird.prototype.numLegs = 2; // prototype property
+// Bird.prototype.numLegs = 2; // prototype property
 
-let duck = new Bird('Donald');
+// let duck = new Bird('Donald');
 
-// ------------------
+// // ------------------
 
-// Add all of the own properties of beagle to the array ownProps. Add all of the prototype properties of Dog to the array prototypeProps.
-function Dog(name) {
-  this.name = name;
-}
+// // Add all of the own properties of beagle to the array ownProps. Add all of the prototype properties of Dog to the array prototypeProps.
+// function Dog(name) {
+//   this.name = name;
+// }
 
-Dog.prototype.numLegs = 4;
+// Dog.prototype.numLegs = 4;
 
-let beagle = new Dog('Snoopy');
+// let beagle = new Dog('Snoopy');
 
-let ownProps = [];
-let prototypeProps = [];
+// let ownProps = [];
+// let prototypeProps = [];
 
-// Only change code below this line
-for (let property in beagle) {
-  if (beagle.hasOwnProperty(property)) {
-    ownProps.push(property);
-  } else {
-    prototypeProps.push(property);
-  }
-}
+// // Only change code below this line
+// for (let property in beagle) {
+//   if (beagle.hasOwnProperty(property)) {
+//     ownProps.push(property);
+//   } else {
+//     prototypeProps.push(property);
+//   }
+// }
 
-console.log(prototypeProps);
+// console.log(prototypeProps);
 
-// --------------
+// // --------------
 
-// constructor
-function Dog(name) {
-  this.name = name;
-}
+// // constructor
+// function Dog(name) {
+//   this.name = name;
+// }
 
-function joinDogFraternity(candidate) {
-  if (candidate.constructor === Dog) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function joinDogFraternity(candidate) {
+//   if (candidate.constructor === Dog) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-// prototype
-// Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving the animal instance the same "recipe" as any other instance of Animal.
+// // prototype
+// // Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving the animal instance the same "recipe" as any other instance of Animal.
+
+// function Animal() {}
+
+// Animal.prototype = {
+//   constructor: Animal,
+//   eat: function () {
+//     console.log('nom nom nom');
+//   },
+// };
+
+// // Only change code below this line
+
+// let a = Object.create(Animal.prototype); // Change this line
+// let b = Object.create(Animal.prototype); // Change this line
 
 function Animal() {}
 
@@ -59,7 +73,9 @@ Animal.prototype = {
   },
 };
 
-// Only change code below this line
+function Dog() {}
 
-let a = Object.create(Animal.prototype); // Change this line
-let b = Object.create(Animal.prototype); // Change this line
+//instances of Dog inherit from Animal.
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
