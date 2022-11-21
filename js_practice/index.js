7798f946b73cdc174f98b2f12f3ea4306b1f5817
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-21 23:31:51 +0200

    Object.create()

--- js_practice/index.js
+++ js_practice/index.js
@@ -1,10 +1,13 @@
-// function Bird(name) {
-//   this.name = name; //own property
-// }
+//  function constructor
+function Bird(name) {
+  this.name = name; //own property
+}
 
-// Bird.prototype.numLegs = 2; // prototype property
+Bird.prototype.numLegs = 2; // prototype property
 
-// let duck = new Bird('Donald');
+let duck = new Bird('Donald');
+
+// ------------------
 
 // Add all of the own properties of beagle to the array ownProps. Add all of the prototype properties of Dog to the array prototypeProps.
 function Dog(name) {
@@ -26,3 +29,37 @@ for (let property in beagle) {
     prototypeProps.push(property);
   }
 }
+
+console.log(prototypeProps);
+
+// --------------
+
+// constructor
+function Dog(name) {
+  this.name = name;
+}
+
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// prototype
+// Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving the animal instance the same "recipe" as any other instance of Animal.
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+};
+
+// Only change code below this line
+
+let a = Object.create(Animal.prototype); // Change this line
+let b = Object.create(Animal.prototype); // Change this line
