f97fa873b7bc5a7c8e86a690c90f867bdf1a13a4
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-22 15:37:16 +0200

    repeating array methods

---
+++ js_practice/array_methods.js
@@ -0,0 +1,52 @@
+const items = [
+  { name: 'Bike', price: 10 },
+  { name: 'Tv', price: 20 },
+  { name: 'Album', price: 30 },
+  { name: 'Book', price: 40 },
+  { name: 'Phone', price: 50 },
+  { name: 'Computer', price: 600 },
+  { name: 'KEyboard', price: 100 },
+  { name: 'Mouse', price: 200 },
+  { name: 'Hearphones', price: 300 },
+];
+const a = [1, 2, 3, 4, 5];
+
+// const filteredItem = items.filter((item) => {
+//   return item.price < 100;
+// });
+// console.log(filteredItem);
+
+// const mapItem = items.map((item) => {
+//   return item.name;
+// });
+// console.log(mapItem);
+
+// const foundItem = items.find((item) => {
+//   return item.name === 'Mouse';
+// });
+// console.log(foundItem);
+
+// items.forEach((item) => {
+//   console.log(item.price);
+// });
+
+// const hasInexpensiveItems = items.some((item) => {
+//   // returns a boolean value
+//   return item.price <= 100;
+// });
+// console.log(hasInexpensiveItems);
+
+// const hasInexpensiveItems = items.every((item) => {
+//   // returns a boolean value
+//   return item.price <= 100;
+// });
+// console.log(hasInexpensiveItems);
+
+// const total = items.reduce((currentTotal, item) => {
+//   return item.price + currentTotal;
+// }, 0);
+// console.log(total);
+
+// const total = a.includes(2);
+// console.log(total);
+// // returns a boolean value
