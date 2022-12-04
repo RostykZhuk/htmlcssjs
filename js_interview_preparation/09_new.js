9e8bafc427d936e3971c8285a4af3473685d8c15
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-12-04 06:59:16 +0200

    new

---
+++ js_interview_preparation/09_new.js
@@ -0,0 +1,16 @@
+function Cat(color, name) {
+  this.color = color;
+  this.name = name;
+}
+// const barny = new Cat('black', 'barny');
+// console.log(barny);
+
+// function myNew(constructor, ...args) {
+//   const obj = {};
+//   Object.setPrototypeOf(obj, constructor.prototype);
+//   return constructor.apply(obj, args) || obj;
+// }
+// const barny = myNew(Cat, 'black', 'barny');
+// console.log(barny);
+
+// ================
