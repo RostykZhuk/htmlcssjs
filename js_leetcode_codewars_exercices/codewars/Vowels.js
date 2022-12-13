b4c04fefb272c8f9bec909d97da14fa5b7642a89
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-12-13 14:08:11 +0200

    Vowels

---
+++ js_leetcode_codewars_exercices/codewars/Vowels.js
@@ -0,0 +1,18 @@
+function getCount(input) {
+  var vowelsCount = 0;
+  var inputLetters = input.split('');
+
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  vowels.forEach(function (vowel) {
+    inputLetters.forEach(function (inpLetter) {
+      if (inpLetter === vowel) {
+        vowelsCount++;
+      }
+    });
+  });
+
+  return vowelsCount;
+}
+
+console.log(getCount('abracadabra'));
