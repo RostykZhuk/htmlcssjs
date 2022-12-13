70d8bf36b55db84e2b024c067a3631ea60d6e8b7
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-12-13 13:09:57 +0200

    string words reverse

---
+++ js_leetcode_codewars_exercices/codewars/reverse string.js
@@ -0,0 +1,10 @@
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map(function (word) {
+      return word.split('').reverse().join('');
+    })
+    .join(' ');
+}
+
+console.log(reverseWords('I am the best boy'));
