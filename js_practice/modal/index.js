c158cbd5f5271368dbfaf39f0127b9c30307bbe7
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-12-15 10:49:30 +0200

    modal part 2

--- js_practice/modal/index.js
+++ js_practice/modal/index.js
@@ -1,1 +1,32 @@
-const modal = $.modal();
+const fruits = [
+  { id: 1, title: 'apples', price: 20 },
+  { id: 2, title: 'Oranges', price: 30 },
+  { id: 3, title: 'Bananas', price: 40 },
+];
+
+const modal = $.modal({
+  title: "Rostyk's Modal",
+  closable: true,
+  content: `  <p>Lorem ipsum dolor sir</p>
+  <p>Lorem ipsum dolor sir</p>`,
+  width: '400px',
+  footerButtons: [
+    {
+      text: 'Okey',
+      type: 'primary',
+      handler() {
+        console.log('Primaty btn clicked');
+        modal.close();
+      },
+    },
+    {
+      text: 'Cancel',
+      type: 'danger',
+      handler() {
+        console.log('Danger btn clicked');
+        modal.close();
+      },
+    },
+  ],
+});
+// all this paramethers are stored in options
