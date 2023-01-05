a86de97afcbc5e3a446fdb7220550497047bde26
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-06 01:30:18 +0200

    calculator

---
+++ js_practice/calculator/math.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  return a + b;
+}
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
